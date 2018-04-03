ca461b38a1ce0e70f1e8fe37f68863ebe1a6df37
Luís Felipe <lipeluiis@hotmail.com>
2018-04-03 14:09:21 -0300

    Fazendo mais um exercício

--- EP/2/ep2.py
+++ EP/2/ep2.py
@@ -37,3 +37,37 @@ def compute_cost(X, y, w):
     J = J/X.shape[0]
 
     return J
+
+# Exercício 3
+
+def compute_wgrad(X, y, w):
+    """
+    Calculates gradient of J(w) with respect to w.
+
+    :param X: design matrix
+    :type X: np.ndarray(shape=(N, d))
+    :param y: regression targets
+    :type y: np.ndarray(shape=(N, 1))
+    :param w: weights
+    :type w: np.array(shape=(d, 1))
+    :return: gradient
+    :rtype: np.array(shape=(d, 1))
+    """
+    
+    eps = 1e-4
+    d = w.shape[0]
+    grad = np.array([])
+    
+    for i in range(0, d):
+        w_mais = np.array(w, copy=True)
+        w_menos = np.array(w, copy=True)
+        
+        w_mais[i] += eps
+        w_menos[i] -= eps
+        
+        J_mais = compute_cost(X, y, w_mais)
+        J_menos = compute_cost(X, y, w_menos)
+
+        grad = np.append(grad, (J_mais - J_menos)/(2*eps)) 
+      
+    return grad
