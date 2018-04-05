944ab6697188f8116ab8977222f33a47d4aeabb2
Luís Felipe <lipeluiis@hotmail.com>
2018-04-05 10:27:33 -0300

    Adicionando exercício 5

--- EP/2/ep2.py
+++ EP/2/ep2.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 # Exercício 1
 
 def standardize(X):
@@ -96,12 +98,48 @@ def batch_gradient_descent(X, y, w, learning_rate, num_iters):
     
     weights_history = [w.flatten()]
     cost_history = [compute_cost(X, y, w)]
-    d = w.shape[0]
 
-    for i in range(d):
+    for i in range(num_iters):
         grad = compute_wgrad(X, y, w)
         w -= learning_rate*grad
-        weights_history.append(w)
+        weights_history.append(w.flatten())
         cost_history.append(compute_cost(X, y, w))
 
     return w, weights_history, cost_history 
+
+#Exercício 5
+
+def stochastic_gradient_descent(X, y, w, learning_rate, num_iters, batch_size):
+    """
+     Performs stochastic gradient descent optimization
+
+    :param X: design matrix
+    :type X: np.ndarray(shape=(N, d))
+    :param y: regression targets
+    :type y: np.ndarray(shape=(N, 1))
+    :param w: weights
+    :type w: np.array(shape=(d, 1))
+    :param learning_rate: learning rate
+    :type learning_rate: float
+    :param num_iters: number of iterations
+    :type num_iters: int
+    :param batch_size: size of the minibatch
+    :type batch_size: int
+    :return: weights, weights history, cost history
+    :rtype: np.array(shape=(d, 1)), list, list
+    """
+    
+    weights_history = [w.flatten()]
+    cost_history = [compute_cost(X, y, w)]
+    N = X.shape[0]
+    
+    for i in range(num_iters):
+        index = np.random.choice(N, batch_size, replace=False)
+        sample_X = X[index,:]
+        sample_y = y[index,:]
+        grad = compute_wgrad(sample_X, sample_y, w)
+        w -= learning_rate*grad
+        weights_history.append(w.flatten())
+        cost_history.append(compute_cost(X, y, w))
+
+    return w, weights_history, cost_history
